2d1108ea6f55b98b82fffbaf4818783982678c36
Олег Архипов <grimglance@mail.ru>
2024-02-02 00:52:16 +0700

    added class with blank props and ctor

---
+++ Range/Range.cs
@@ -0,0 +1,22 @@
+﻿namespace Range;
+
+public class Range
+{
+    public double From { get; set; }
+    public double To { get; set; }
+
+    public Range()
+    {
+
+    }
+
+    public double GetLength()
+    {
+        return 0;
+    }
+
+    public bool IsInside()
+    {
+        return true;
+    }
+}
