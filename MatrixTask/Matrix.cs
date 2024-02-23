90c54fca43efeae7a96c58ae01aad0a448ad019c
Serge <grimglance@gmail..com>
2024-02-24 01:56:31 +0700

    wtf is goin on

--- MatrixTask/Matrix.cs
+++ MatrixTask/Matrix.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MatrixTask;
 
-namespace CsvTask;
-internal class CsvParser
+public class Matrix
 {
 }
