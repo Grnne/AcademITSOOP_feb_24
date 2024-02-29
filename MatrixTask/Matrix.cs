3a745b55f9d4327c624824609cc75a027b6faf98
Олег Архипов <grimglance@mail.ru>
2024-02-29 17:51:18 +0700

    maybe this time

--- MatrixTask/Matrix.cs
+++ MatrixTask/Matrix.cs
@@ -1,5 +1,10 @@
-﻿namespace MatrixTask;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Matrix
+namespace CsvTask;
+internal class CsvParser
 {
 }
