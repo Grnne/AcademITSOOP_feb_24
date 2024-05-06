5b1245637986d908c6424d5b4011ccd90de11f4d
Grnne <95399645+Grnne@users.noreply.github.com>
2024-05-06 10:31:23 +0700

    Delete MinesweeperTaskBroken directory

--- MinesweeperTaskBroken/Model/IModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MinesweeperTask.Model
-{
-    public interface IModel
-    {
-        Minefield Minefield { get; set; }
-
-        void InitBoard(int rowsAmount, int columnsAmount, int bombsAmount);
-    }
-}
